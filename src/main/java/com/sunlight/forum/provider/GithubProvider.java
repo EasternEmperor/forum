fdf4439dc9b412f14638c2bb2b596d551198867e
EasternEmperor <1908355690@qq.com>
2024-05-07 23:16:07 +0800

    application properties

---
+++ src/main/java/com/sunlight/forum/provider/GithubProvider.java
@@ -0,0 +1,50 @@
+package com.sunlight.forum.provider;
+
+import com.alibaba.fastjson2.JSON;
+import com.sunlight.forum.dto.AccessTokenDTO;
+import com.sunlight.forum.dto.GithubUser;
+import okhttp3.*;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+/**
+ * Created by ee on 2024/5/5.
+ */
+@Component
+public class GithubProvider {
+    public String getAccessToken(AccessTokenDTO accessTokenDTO) {
+        MediaType mediaType = MediaType.get("application/json; charset=utf-8");
+        OkHttpClient client = new OkHttpClient();
+
+        RequestBody body = RequestBody.create(JSON.toJSONString(accessTokenDTO), mediaType);
+        Request request = new Request.Builder()
+                .url("https://github.com/login/oauth/access_token")
+                .post(body)
+                .build();
+        try (Response response = client.newCall(request).execute()) {
+            String string = response.body().string();
+            String token = string.split("&")[0].split("=")[1];
+            return token;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    // 通过accesstoken向github发送请求，获取用户信息
+    public GithubUser getUser(String accessToken) {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url("https://api.github.com/user?access_token=" + accessToken)
+                .build();
+        try (Response response = client.newCall(request).execute()) {
+            String string = response.body().string();
+            System.out.println(string);
+            GithubUser githubUser = JSON.parseObject(string, GithubUser.class);
+            return githubUser;
+        } catch (IOException e) {
+        }
+        return null;
+    }
+}
